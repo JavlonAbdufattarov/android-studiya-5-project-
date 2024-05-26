3d59c9528b918a3687fffb0cacaea254686c8a54
Javlon <abdufattarovjavlon@gmail.com>
2024-05-26 12:40:38 +0300

    for Lesson 7

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mediaPlayer: MediaPlayer
@@ -16,20 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val playButton: Button = findViewById(R.id.button)
-        mediaPlayer = MediaPlayer.create(this, R.raw.sample3s) // Замените your_sound_file на имя вашего файла
+        playButton.setOnClickListener{
+            val intent = Intent(this ,QuizStorage::class.java)
+            startActivity(intent)
 
-        playButton.setOnClickListener {
-            if (mediaPlayer.isPlaying) {
-                mediaPlayer.pause()
-                mediaPlayer.seekTo(0)
-            } else {
-                mediaPlayer.start()
-            }
+
         }
+
+
     }
 
-    override fun onDestroy() {
-        mediaPlayer.release()
-        super.onDestroy()
-    }
+
 }
---
+++ app/src/main/java/com/example/myapplication/ResultActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val tv_score = findViewById<TextView>(R.id.tv_score)
+        val btn_finish = findViewById<Button>(R.id.btn_finish)
+
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tv_score.text = "Your Score is $correctAnswers out of $totalQuestions."
+
+        btn_finish.setOnClickListener {
+            startActivity(Intent(this@ResultActivity, MainActivity::class.java))
+        }
+    }
+}
